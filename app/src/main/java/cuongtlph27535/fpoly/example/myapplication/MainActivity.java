b4f03647636de06438455138db2b35033560fef4
Admin <cuongtlph27535>
2022-11-11 14:43:25 +0700

    cmc

---
+++ app/src/main/java/cuongtlph27535/fpoly/example/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package cuongtlph27535.fpoly.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
